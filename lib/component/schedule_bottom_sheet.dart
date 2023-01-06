8d1dcedafe8b408d57f97eb5a5839d8d888fb397
fluorjo <fluorjo@naver.com>
2023-01-07 05:47:25 +0900

    customTextField 구현

--- lib/component/schedule_bottom_sheet.dart
+++ lib/component/schedule_bottom_sheet.dart
@@ -1,4 +1,6 @@
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/component/custom_text_field.dart';
 
 class ScheduleBottomSheet extends StatefulWidget {
   const ScheduleBottomSheet({Key? key}) : super(key: key);
@@ -14,7 +16,50 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
       child: Container(
         height: MediaQuery.of(context).size.height / 2,
         color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
+          child: Column(children: [
+            Row(
+              children: const [
+                Expanded(
+                  child: CustomTextField(
+                    label: '시작 시간',
+                    isTime: true,
+                  ),
+                ),
+                SizedBox(
+                  width: 16.0,
+                ),
+                Expanded(
+                  child: CustomTextField(
+                    label: '종료 시간',
+                    isTime: true,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 8.0),
+            const Expanded(
+              child: CustomTextField(
+                label: '내용',
+                isTime: false,
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: onSavePressed,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: PRIMARY_COLOR,
+                ),
+                child: const Text('저장'),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
+
+  void onSavePressed() {}
 }
