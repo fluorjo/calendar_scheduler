a74094d6144d1da8bd427a5aebd855cd99cd9a77
fluorjo <fluorjo@naver.com>
2023-01-06 20:06:36 +0900

    클릭시 색 변경

--- lib/components/main_calendar.dart
+++ lib/components/main_calendar.dart
@@ -3,9 +3,22 @@ import 'package:table_calendar/table_calendar.dart';
 import 'package:calendar_scheduler/const/colors.dart';
 
 class MainCalendar extends StatelessWidget {
+  final OnDaySelected onDaySelected;
+  final DateTime selectedDate;
+
+  const MainCalendar({
+    required this.onDaySelected,
+    required this.selectedDate,
+  });
+
   @override
   Widget build(BuildContext context) {
     return TableCalendar(
+      onDaySelected: onDaySelected,
+      selectedDayPredicate: (date) =>
+          date.year == selectedDate.year &&
+          date.month == selectedDate.month &&
+          date.day == selectedDate.day,
       firstDay: DateTime(1800, 1, 1),
       lastDay: DateTime(3000, 1, 1),
       focusedDay: DateTime.now(),
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:calendar_scheduler/components/main_calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime selectedDate = DateTime.utc(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(children: [
-          MainCalendar(),
+          MainCalendar(
+            selectedDate: selectedDate,
+            onDaySelected: onDaySelected,
+          ),
         ]),
       ),
     );
+  }
+
+  void onDaySelected(DateTime seletedDate, DateTime focusedDate) {
+    setState(() {
+      selectedDate = seletedDate;
+    });
   }
 }
