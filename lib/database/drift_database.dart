44ff3884926a3d6e2f04863cc3f1493991bb19ca
fluorjo <fluorjo@naver.com>
2023-01-07 14:08:18 +0900

    쿼리 구현

--- lib/database/drift_database.dart
+++ lib/database/drift_database.dart
@@ -1,11 +1,39 @@
 import 'package:calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'dart:io';
+part 'drift_database.g.dart';  // part 파일 지정
 
-part 'drift_database.g.dart';
 
 @DriftDatabase(
   tables: [
     Schedules,
   ],
 )
-class LocalDatabase extends _$LocalDatabase {}
+class LocalDatabase extends _$LocalDatabase {
+
+    LocalDatabase() : super(_openConnection());
+
+  Stream<List<Schedule>> watchSchedules(DateTime date) =>
+      (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
+
+    Future<int> createSchedule(SchedulesCompanion data) =>
+      into(schedules).insert(data);
+
+      
+  Future<int> removeSchedule(int id) =>
+      (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+
+  int get schemaVersion => 1;
+}
+
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory(); // ➊ 데이터베이스 파일 저장할 폴더
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
